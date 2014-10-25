5d82b6aa944db5240b8bdac2b0c373d010106690
nrakochy <nick.rakochy@gmail.com>
2014-10-25 15:02:41 -0500

    Updated Display and display_spec with smaller methods and more test cases

--- lib/display.rb
+++ lib/display.rb
@@ -1,4 +1,5 @@
 require_relative 'visual_rep'
+require 'pry'
 class Display
 
   def empty_board height = 3, width = 3
@@ -12,76 +13,84 @@ class Display
     space
   end
   
-  def modified_board player1_moves = [], player2_moves = [], player1_symbol = 'X', player2_symbol = 'O'
-    board = empty_board
+  def modified_board player1_moves = [], player2_moves = [], height = 3, width = 3, player1_symbol = 'X', player2_symbol = 'O'
+    board = empty_board(height, width)
     player1_moves.each{|move| board[move - 1].visual_symbol = player1_symbol}
     player2_moves.each{|move| board[move - 1].visual_symbol = player2_symbol}
     board
   end
 
-  def non_terminating_row_space_less_than_ten_width spaces, index
-    "  " + "#{spaces[index].visual_symbol} " + "  " 
+  def non_terminating_row_space_less_than_ten_width space 
+    "  " + "#{space.visual_symbol} " + "  " + "|"
   end
 
-  def terminating_row_space_less_than_ten_width spaces, index
-    "  " + "#{spaces[index].visual_symbol} " + "  " 
+  def terminating_row_space_less_than_ten_width space
+    "  " + "#{space.visual_symbol} " + "  \n"
   end
 
-  def non_terminating_row_space_more_than_ten_width spaces, index
-     "  " + "#{spaces[index].visual_symbol}" + "  " 
+  def non_terminating_row_space_more_than_ten_width space
+     "  " + "#{space.visual_symbol}" + "  " + "|"
   end
 
-  def terminating_row_space_more_than_ten_width spaces, index
-     "  " + "#{spaces[index].visual_symbol}" + "  " 
+  def terminating_row_space_more_than_ten_width space
+     "  " + "#{space.visual_symbol}" + "  \n"
   end
 
-  def horizontal_line width, display
+  def add_non_terminating_row_space row_space
+    if row_space.visual_symbol.to_s.length <  2
+       non_terminating_row_space_less_than_ten_width(row_space)
+    else 
+        non_terminating_row_space_more_than_ten_width(row_space)
+    end
+  end
+
+  def add_terminating_row_space row_space
+    if row_space.visual_symbol.to_s.length <  2
+        terminating_row_space_less_than_ten_width(row_space)
+    else 
+        terminating_row_space_more_than_ten_width(row_space)
+    end
+  end
+
+  def horizontal_line width
     display = []
     (width * 7).times{|horizontal| display << "_"}
+    display << "\n"
     return_string(display)
-  end
-
-  def new_line
-    "\n"
-  end
-
-  def vertical_line
-    "|"
   end
 
   def return_string display
     display.inject(""){|sum, string| sum += string}
   end
+  
+  def row_builder board, width
+    board.slice(0, width)
+  end
 
-  def create_display_row width, spaces, index
+
+  def create_display_row row_spaces
     display = []
-    x = 1 
-    (width-1).times do |one_space_at_a_time| 
-      if  x < 9
-        display << non_terminating_row_space_less_than_ten_width(spaces, index)
-        display << vertical_line
-        x += 1
-      else 
-        display << non_terminating_row_space_more_than_ten_width(spaces, index)
-        display << vertical_line
-      end
-    end  
-    width < 10 ? display << terminating_row_space_less_than_ten_width(space, index) : display << terminating_row_space_more_than_ten_width(space, index) 
-    display << new_line
+    last_space = row_spaces.pop
+    row_spaces.each{|row_space| display << add_non_terminating_row_space(row_space)}
+    display << add_terminating_row_space(last_space)
     return_string(display)
   end
 
-  def visual_board player1_moves = [], player2_moves = [], player1_symbol = 'X', player2_symbol = 'O', height = 3, width = 3
-    current_board = modified_board(player1_moves, player2_moves, player1_symbol, player2_symbol)
+  def visual_board player1_moves = [], player2_moves = [], height = 3, width = 3, player1_symbol = 'X', player2_symbol = 'O'
+    display = []
+    current_board = modified_board(player1_moves, player2_moves, height, width, player1_symbol, player2_symbol)
     x = 1
     display = []
     while x < height
-      display << create_display_row
-      display << horizontal_line
+      row_builder = row_builder(current_board, width)
+      current_board -= row_builder
+      display << create_display_row(row_builder)
+      display << horizontal_line(width)
       x += 1
+      current_board
     end
-    display << create_display_row
-    print return_string(display)
+    display << create_display_row(current_board)
+    return_string(display)
   end
 end
 
--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -51,44 +51,188 @@ describe Display do
   end
 
   describe '#visual_board' do
-    context '3x3 board' do
-      xit 'returns a 3x3 board filled with integers as visual represenatation of board spaces' do
+    context 'various board sizes' do
+      it 'returns a 3x3 board filled with integers as visual represenatation of board spaces' do
         board = Display.new.visual_board
-        expect(board).to eq('')
+        board3 =  "  1   |  2   |  3   "  + "\n" +
+                  "_____________________" + "\n" +
+                  "  4   |  5   |  6   "  + "\n" +
+                  "_____________________" + "\n" +
+                  "  7   |  8   |  9   "  + "\n" 
+        expect(board).to eq(board3)
       end
+      it 'returns a 4x4 board filled with integers as visual represenatation of board spaces' do
+        player1_moves = []
+        player2_moves = []
+        width = 4
+        height = 4
+        board = Display.new.visual_board(player1_moves, player2_moves,height, width)
+        board4 =  "  1   |  2   |  3   |  4   "  + "\n" +
+                  "____________________________" + "\n" +
+                  "  5   |  6   |  7   |  8   "  + "\n" +
+                  "____________________________" + "\n" +
+                  "  9   |  10  |  11  |  12  "  + "\n" + 
+                  "____________________________" + "\n" +
+                  "  13  |  14  |  15  |  16  " + "\n" 
+        expect(board).to eq(board4)
+      end
+
+      it 'returns a 4x4 board filled with integers and Player symbols already played as visual represenatation of board spaces' do
+        player1_moves = [10, 11]
+        player2_moves = [14, 15]
+        width = 4
+        height = 4
+        board = Display.new.visual_board(player1_moves, player2_moves,height, width)
+        board4 =  "  1   |  2   |  3   |  4   "  + "\n" +
+                  "____________________________" + "\n" +
+                  "  5   |  6   |  7   |  8   "  + "\n" +
+                  "____________________________" + "\n" +
+                  "  9   |  X   |  X   |  12  "  + "\n" + 
+                  "____________________________" + "\n" +
+                  "  13  |  O   |  O   |  16  " + "\n" 
+        expect(board).to eq(board4)
+      end
+
+      it 'returns properly formatted 4x4 board with custom sized player symbols included' do
+        player1_moves = [10, 11]
+        player2_moves = [14, 15]
+        width = 4
+        height = 4
+        player1_symbol = "&"
+        player2_symbol = "~+"
+        board = Display.new.visual_board(player1_moves, player2_moves,height, width, player1_symbol, player2_symbol)
+        board4 =  "  1   |  2   |  3   |  4   "  + "\n" +
+                  "____________________________" + "\n" +
+                  "  5   |  6   |  7   |  8   "  + "\n" +
+                  "____________________________" + "\n" +
+                  "  9   |  &   |  &   |  12  "  + "\n" + 
+                  "____________________________" + "\n" +
+                  "  13  |  ~+  |  ~+  |  16  " + "\n" 
+        expect(board).to eq(board4)
+      end
+
+      it 'returns a 5x5 board filled with integers as visual represenatation of board spaces' do
+        player1_moves = []
+        player2_moves = []
+        width = 5
+        height = 5
+        board = Display.new.visual_board(player1_moves, player2_moves,height, width)
+        board5 =  "  1   |  2   |  3   |  4   |  5   "  + "\n" +
+                  "___________________________________" + "\n" +
+                  "  6   |  7   |  8   |  9   |  10  "  + "\n" +
+                  "___________________________________" + "\n" +
+                  "  11  |  12  |  13  |  14  |  15  "  + "\n" +
+                  "___________________________________" + "\n" +
+                  "  16  |  17  |  18  |  19  |  20  " + "\n" +
+                  "___________________________________" + "\n" +
+                  "  21  |  22  |  23  |  24  |  25  " + "\n"
+        expect(board).to eq(board5)
+      end
+
+      it 'returns a 2x4 board filled with integers as visual represenatation of board spaces' do
+        player1_moves = []
+        player2_moves = []
+        width = 4
+        height = 2
+        board = Display.new.visual_board(player1_moves, player2_moves,height, width)
+        board2x4 =  "  1   |  2   |  3   |  4   "  + "\n" +
+                    "____________________________" + "\n" +
+                    "  5   |  6   |  7   |  8   "  + "\n"
+        expect(board).to eq(board2x4)
+      end
+
+
+
     end
   end
 
   describe 'Individual board space builder methods' do
     context 'Board width < 10' do
-      it 'returns a representative space that is not at the end of a row' do 
+      it '#non_terminating_row_space_less_than_ten_width returns a representative space that is not at the end of a row' do 
         space1 = VisualRep.new(1)
-        visual = "  1   "
-        expect(Display.new.non_terminating_row_space_less_than_ten_width([space1],0)).to eq(visual)
+        visual = "  1   |"
+        expect(Display.new.non_terminating_row_space_less_than_ten_width(space1)).to eq(visual)
       end
+
+      it '#add_non_terminating_row_space returns a representative space when string on VisualRep.visual_symbol is >= 2' do
+        spaceX = VisualRep.new("X")
+        visual =  '  X   |'
+        expect(Display.new.non_terminating_row_space_less_than_ten_width(spaceX)).to eq(visual)
+      end
+
     end
 
     context 'Board width >= 10' do
-      it 'returns a representative space that is not at the end of a row' do 
+      it '#non_terminating_row_space_more_than_ten_width returns a representative space that is not at the end of a row' do 
         space10 = VisualRep.new(10)
-        visual =  '  10  '
-        expect(Display.new.non_terminating_row_space_more_than_ten_width([space10], 0)).to eq(visual)
-      end
-    end
-    context 'Board size agnostic' do
-      it '#vertical_line returns a vertical line break in between a space' do
-        vertical = '|'
-        expect(Display.new.vertical_line).to eq(vertical)
+        visual =  '  10  |'
+        expect(Display.new.non_terminating_row_space_more_than_ten_width(space10 )).to eq(visual)
       end
 
+      it '#add_non_terminating_row_space returns a representative space when string on VisualRep.visual_symbol is >= 2' do
+        space10 = VisualRep.new(10)
+        visual =  '  10  |'
+        expect(Display.new.non_terminating_row_space_more_than_ten_width(space10 )).to eq(visual)
+      end
+
+    end
+
+    context 'Board size agnostic' do
       it '#return_string returns a string of space components from an array ' do
         arr = ['h', 'e', 'l', 'l', 'o']
         expect(Display.new.return_string(arr)).to eq('hello')
       end
 
-      xit '#create_display_row returns a row of TTT board' do
+      it '#horizontal_line returns a horizontal line that is the width of a single space * the width of the board + width-1 (for the number of vertical lines) ' do
+        board_width = 3
+        line = "_____________________\n"
+        expect(Display.new.horizontal_line(board_width)).to eq(line)
+      end
+
+      it '#row_builder returns an array with width number of elements' do
+        board = [1,2,3,4,5,6]
+        width = 3
+        expect(Display.new.row_builder(board, width)).to eq([1,2,3])
       end
     end
   end
 
+  describe '#create_display_row' do
+    context 'Board size agnostic' do
+      it 'returns a string-row of TTT board' do
+        board_spaces= [1,2,3]
+        board_spaces.map!{|num| VisualRep.new(num)}
+        board_row = "  1   |  2   |  3   \n"
+        expect(Display.new.create_display_row(board_spaces)).to eq(board_row)
+      end
+
+      it 'returns a string-row that does not start with 1' do
+        board_spaces = [3,4,5]
+        board_spaces.map!{|num| VisualRep.new(num)}
+        board_row = "  3   |  4   |  5   \n"
+        expect(Display.new.create_display_row(board_spaces)).to eq(board_row)
+      end
+
+      it 'returns a string-row that contains single and double digit numbers' do
+        board_spaces = [9,10,11]
+        board_spaces.map!{|num| VisualRep.new(num)}
+        board_row = "  9   |  10  |  11  \n"
+        expect(Display.new.create_display_row(board_spaces)).to eq(board_row)
+      end   
+
+      it 'returns a string-row that formats row with only double digit correctly' do
+        board_spaces = [10,11,12,13]
+        board_spaces.map!{|num| VisualRep.new(num)}
+        board_row = "  10  |  11  |  12  |  13  \n"
+        expect(Display.new.create_display_row(board_spaces)).to eq(board_row)
+      end
+
+      it 'returns a string-row that formats double digit and Player symbol correctly' do
+        board_spaces = [10,"X",12,13]
+        board_spaces.map!{|num| VisualRep.new(num)}
+        board_row = "  10  |  X   |  12  |  13  \n"
+        expect(Display.new.create_display_row(board_spaces)).to eq(board_row)
+      end
+    end
+  end
 end
